e91536375306306133b0b492cbed34f2915cae1c
Michael Zhang <failed.down@gmail.com>
2015-12-13 17:23:15 +0000

    team captain

--- api/team.js
+++ api/team.js
@@ -154,7 +154,7 @@ exports.get_all_teams = function(callback) {
 				async.each(members, function(member, callback2) {
 					user.get_user_data(member, function(userdata) {
 						delete userdata["expire"];
-						userdata["captain"] = userdata["username_lower"] == team["captain"].toLowerCase();
+						// userdata["captain"] = userdata["username_lower"] == 
 						teammembers.push(userdata);
 						callback2();
 					});
